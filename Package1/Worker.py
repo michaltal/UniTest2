0dce1f1f590a9e875f1c58415021cf32c3f8483a
michaltal <michaltal1@gmail.com>
2020-07-18 14:58:01 +0300

    removed unnecessary modules

--- Package1/Worker.py
+++ Package1/Worker.py
@@ -35,12 +35,10 @@ class Worker:
         return f'{self.first_name} says hello to {other.first_name}'
 
     def location(self):
-        #response = requests.get(f'https://geocode.xyz/?locate='{self.address}', '{self.cntry}' & json = 1')
+        """Returns the location coordinates of the worker's address"""
         param = self.address + ',' + self.cntry
-        #print(param)
         url = f'https://geocode.xyz/?locate={param} &json=1'
-        #url = "https://geocode.xyz/?locate=2 Dizengof, Tel Aviv, il&json=1"
-        #print(url)
+
         payload = {}
         headers = {
           'Cookie': '__cfduid=db7a3a4e2ae670bba0d182afaf4d2cc2f1592807975; xyzh=xyzh'
@@ -53,25 +51,3 @@ class Worker:
             return response.text
         else:
             return 'Bad response!'
-
-
-
-#bob = Worker('Bob', 'Marshall', 1970, 7, 30, '2 Dizengof, Tel Aviv', 'il')
-#print(bob.location())
-#print(bob.full_name())
-#print (bob.days_to_birthday())
-# print(bob.first_name, 'birth year is ', bob.birth_year,'\n',bob.age())
-# print('Number of days to birthday: ',bob.days_to_birthday())
-# print(datetime.datetime.now().year)
-# print(datetime.datetime.today())
-# alice = Worker('Alice', 'Smith',1995)
-# print(bob.full_name(),bob.birth_year)
-# print(alice.full_name())
-# print(f'{bob.full_name()} is {bob.age()} years old.')
-# print(bob.greet(alice))
-
-
-# bob.country = 'Israel'
-# bob.city = 'Haifa'
-# print(f'{bob.full_name()} is from {bob.city}, {bob.country}')
-
--- Package1/test_Worker.py
+++ Package1/test_Worker.py
@@ -1,6 +1,4 @@
 from unittest import TestCase
-from unittest.mock import Mock
-from unittest.mock import MagicMock
 from unittest.mock import patch
 from Package1.Worker import Worker
 
